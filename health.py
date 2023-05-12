bbb088f1ce9a699bf8528701609fd01ac5520f2f
tosicky <tosingeeboy@yahoo.com>
2023-05-12 11:56:56 -0300

    Update health.py

--- health.py
+++ health.py
@@ -2,7 +2,6 @@ import requests, sys
 
 try:
     app_status = requests.get("http://"+ sys.argv[1] + "/api/test/all")
-#     app_status = requests.get("http://localhost:8000/api/test/all")
 
     if app_status.status_code == 200:
         print("App is up and running. API calls can be made.")
